d78dea5fbed2e984337b650bfa0a1e4c7f295e76
tassi-yuzukko <tassi.yuzukko@gmail.com>
2018-03-27 16:07:52 +0900

    プロジェクト ファイルを追加します。

---
+++ TcpTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
